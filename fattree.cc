939ca20df21ac86fda95b7e5af9e5114e6a7a5a0
Daniel Josué <daniel.cubillost@usm.cl>
2025-10-05 16:46:36 -0300

    ADD ECN

--- fattree.cc
+++ fattree.cc
@@ -5,6 +5,7 @@
 #include "ns3/csma-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/netanim-module.h"
+#include "ns3/traffic-control-module.h"
 
 using namespace ns3;
 
@@ -26,46 +27,65 @@ int main(int argc, char *argv[])
   NodeContainer switches;
   switches.Create(2);
 
+  // Habilitar ECN globalmente
+  Config::SetDefault("ns3::TcpL4Protocol::SocketType", StringValue("ns3::TcpNewReno"));
+  Config::SetDefault("ns3::TcpSocketBase::UseEcn", StringValue("On"));
+
   // Internet stack en hosts
   InternetStackHelper stack;
   stack.Install(hosts);
   stack.Install(switches);
 
-  // Canal (CSMA como red LAN entre cada switch y hosts)
-  CsmaHelper csma;
-  csma.SetChannelAttribute("DataRate", StringValue("100Mbps"));
-  csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
+  // Configurar Point-to-Point con soporte ECN
+  PointToPointHelper p2p;
+  p2p.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+  p2p.SetChannelAttribute("Delay", StringValue("2ms"));
 
-  // --- Conectar hosts 0 y 1 al switch 0
-  NodeContainer net1;
-  net1.Add(hosts.Get(0));
-  net1.Add(hosts.Get(1));
-  net1.Add(switches.Get(0));
-  NetDeviceContainer devNet1 = csma.Install(net1);
+  // Configurar cola RED con ECN
+  TrafficControlHelper tchRed;
+  tchRed.SetRootQueueDisc("ns3::RedQueueDisc",
+                          "LinkBandwidth", StringValue("100Mbps"),
+                          "LinkDelay", StringValue("2ms"),
+                          "UseEcn", BooleanValue(true),
+                          "MinTh", DoubleValue(5),
+                          "MaxTh", DoubleValue(15));
 
-  // --- Conectar hosts 2 y 3 al switch 1
-  NodeContainer net2;
-  net2.Add(hosts.Get(2));
-  net2.Add(hosts.Get(3));
-  net2.Add(switches.Get(1));
-  NetDeviceContainer devNet2 = csma.Install(net2);
+  // --- Conectar hosts al switch 0 (enlaces separados P2P)
+  NetDeviceContainer devHost0ToSw0 = p2p.Install(hosts.Get(0), switches.Get(0));
+  NetDeviceContainer devHost1ToSw0 = p2p.Install(hosts.Get(1), switches.Get(0));
+  
+  // Instalar colas RED en enlaces hacia switch 0
+  tchRed.Install(devHost0ToSw0);
+  tchRed.Install(devHost1ToSw0);
+
+  // --- Conectar hosts al switch 1 (enlaces separados P2P)
+  NetDeviceContainer devHost2ToSw1 = p2p.Install(hosts.Get(2), switches.Get(1));
+  NetDeviceContainer devHost3ToSw1 = p2p.Install(hosts.Get(3), switches.Get(1));
+  
+  // Instalar colas RED en enlaces hacia switch 1
+  tchRed.Install(devHost2ToSw1);
+  tchRed.Install(devHost3ToSw1);
 
   // --- Conectar switch 0 <-> switch 1
-  NodeContainer core;
-  core.Add(switches.Get(0));
-  core.Add(switches.Get(1));
-  NetDeviceContainer devCore = csma.Install(core);
+  NetDeviceContainer devCore = p2p.Install(switches.Get(0), switches.Get(1));
+  tchRed.Install(devCore);
 
   // Asignación de direcciones IP
   Ipv4AddressHelper address;
 
   address.SetBase("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer iface1 = address.Assign(devNet1);
-
+  Ipv4InterfaceContainer ifaceHost0 = address.Assign(devHost0ToSw0);
+  
   address.SetBase("10.1.2.0", "255.255.255.0");
-  Ipv4InterfaceContainer iface2 = address.Assign(devNet2);
+  Ipv4InterfaceContainer ifaceHost1 = address.Assign(devHost1ToSw0);
 
   address.SetBase("10.1.3.0", "255.255.255.0");
+  Ipv4InterfaceContainer ifaceHost2 = address.Assign(devHost2ToSw1);
+  
+  address.SetBase("10.1.4.0", "255.255.255.0");
+  Ipv4InterfaceContainer ifaceHost3 = address.Assign(devHost3ToSw1);
+
+  address.SetBase("10.1.5.0", "255.255.255.0");
   Ipv4InterfaceContainer ifaceCore = address.Assign(devCore);
 
   // --- Aplicación: host0 envía a host3
@@ -74,7 +94,7 @@ int main(int argc, char *argv[])
   serverApps.Start(Seconds(1.0));
   serverApps.Stop(Seconds(10.0));
 
-  UdpEchoClientHelper echoClient(iface2.GetAddress(1), 9); // host3 está en net2
+  UdpEchoClientHelper echoClient(ifaceHost3.GetAddress(0), 9); // Dirección actualizada
   echoClient.SetAttribute("MaxPackets", UintegerValue(5));
   echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
   echoClient.SetAttribute("PacketSize", UintegerValue(1024));
@@ -99,7 +119,7 @@ int main(int argc, char *argv[])
   anim.SetConstantPosition(switches.Get(1), 130, 70); // Switch 1
 
   std::cout << "=== Configuración completada, iniciando simulación ===" << std::endl;
-  std::cout << "Host0 (10.1.1.1) enviará 5 paquetes a Host3 (10.1.2.2)" << std::endl;
+  std::cout << "Host0 (" << ifaceHost0.GetAddress(0) << ") enviará 5 paquetes a Host3 (" << ifaceHost3.GetAddress(0) << ")" << std::endl;
 
   Simulator::Stop(Seconds(11.0));
   Simulator::Run();
